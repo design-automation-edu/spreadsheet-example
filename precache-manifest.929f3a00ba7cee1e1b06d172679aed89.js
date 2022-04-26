97cb9f384216c6a42a78bb4a4a43f0d7662b8de5
phtj <patrick@janssen.name>
2022-04-26 11:11:53 +0800

    Updates

---
+++ precache-manifest.929f3a00ba7cee1e1b06d172679aed89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231585850ce978899c5aea779c8e7b1b",
+    "url": "/spreadsheet-example/index.html"
+  },
+  {
+    "revision": "aa71d033a27923c4e5e1",
+    "url": "/spreadsheet-example/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "94825c95adeaab37c666",
+    "url": "/spreadsheet-example/static/js/2.87620317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spreadsheet-example/static/js/2.87620317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa71d033a27923c4e5e1",
+    "url": "/spreadsheet-example/static/js/main.b6eb22e7.chunk.js"
+  },
+  {
+    "revision": "98946b157a799acb6b1f",
+    "url": "/spreadsheet-example/static/js/runtime-main.4f108e17.js"
+  }
+]);
